c25d63b23359cf75951fc5d7380da70a06e530f2
geniein <cubya0104@gmail.com>
2024-07-14 14:43:48 +0900

    [feature] Add typescript config

--- gatsby-config.js
+++ gatsby-config.js
@@ -39,5 +39,12 @@ module.exports = {
         icon: `src/images/gatsby-icon.png`, // This path is relative to the root of the site.
       },
     },
+    {
+      resolve: 'gatsby-plugin-typescript',
+      options: {
+        isTSX: true,
+        allExtensions: true,
+      },
+    },
   ],
 }
